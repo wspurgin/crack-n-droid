0abe431983bbf36bbfeb1aaeb42f4cb368fa40ba
Will Spurgin <wspurgin@smu.edu>
2014-11-12 13:11:58 -0600

    Initial android app.

---
+++ app/src/androidTest/java/com/kraqen/crackn/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.kraqen.crackn;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
