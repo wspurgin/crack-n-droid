baea75ab0102e62d9e81ceb15cee366d2a87727a
Will Spurgin <wspurgin@smu.edu>
2014-11-13 00:01:32 -0600

    Added Main activity
    Modified some exisiting configurations.

--- app/src/main/java/com/kraqen/crackn/LoginActivity.java
+++ app/src/main/java/com/kraqen/crackn/LoginActivity.java
@@ -42,6 +42,7 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
     private static final String[] DUMMY_CREDENTIALS = new String[]{
             "foo@example.com:hello", "bar@example.com:world"
     };
+
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
      */
